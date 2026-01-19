1ed20dd506d909f26c211dfff2e5b214f7537eb5
George Tsimpinis <gtsimpinis@b-k.gr>
2026-01-19 11:58:05 +0200

    feat: Introduce `fcb.h` defining the `Fcb` structure for Flash Circular Buffer logistics.

--- fcb/fcb.h
+++ fcb/fcb.h
@@ -1,9 +1,21 @@
 #ifndef FCB_H
 #define FCB_H
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 
+/**
+ * @brief FCB Logistics Structure
+ *
+ * This structure holds the configuration and runtime state of the
+ * Flash Circular Buffer.
+ */
+typedef struct {
+  uint32_t first_sector; /**< First sector index used by this FCB instance */
+  uint32_t last_sector;  /**< Last sector index used by this FCB instance */
+  uint32_t sector_size;  /**< Size of each sector in bytes */
+  uint32_t current_sector_id; /**< Monotonic ID of the current active sector */
+} Fcb;
 
 #endif // FCB_H
