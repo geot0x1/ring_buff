6ec417141be8a12b251ca0da6f75eba07200ab84
George Tsimpinis <gtsimpinis@b-k.gr>
2026-01-19 13:18:36 +0200

    feat: Initialize and mount the FCB, then display its initial state.

--- main.c
+++ main.c
@@ -1,21 +1,31 @@
-#include <stdio.h>
 #include "fcb.h"
 #include "flash_mem.h"
+#include <stdio.h>
+#include <string.h>
+
+
+Fcb fcb = {
+    .first_sector = 0, .last_sector = 63, .sector_size = FLASH_SECTOR_SIZE};
 
 int main() {
-    printf("Hello from CMake and GCC!\n");
-    flash_full_erase();
+  printf("Hello from FCB Test!\n");
+  flash_full_erase();
 
-    printf("Flash emulation initialized: %d sectors of %d KB each (%d MB total)\n", 
-           FLASH_SECTOR_COUNT, FLASH_SECTOR_SIZE / 1024, FLASH_SIZE / (1024 * 1024));
-    
-    // Demonstrate write/read
-    char test_data[] = "Flash Test Data";
-    flash_write(0x100, test_data, sizeof(test_data));
-    
-    char read_buffer[32];
-    flash_read(0x100, read_buffer, sizeof(read_buffer));
-    printf("Read from flash: %s\n", read_buffer);
+  printf(
+      "Flash emulation initialized: %d sectors of %d KB each (%d MB total)\n",
+      FLASH_SECTOR_COUNT, FLASH_SECTOR_SIZE / 1024, FLASH_SIZE / (1024 * 1024));
 
-    return 0;
+  int rc = fcb_mount(&fcb);
+  if (rc != 0) {
+    printf("FCB mount failed with code %d\n", rc);
+    return rc;
+  }
+
+  printf("FCB mounted successfully!\n");
+  printf("Current Sector ID: %u\n", fcb.current_sector_id);
+  printf("Write Addr:  0x%08X\n", fcb.write_addr);
+  printf("Read Addr:   0x%08X\n", fcb.read_addr);
+  printf("Delete Addr: 0x%08X\n", fcb.delete_addr);
+
+  return 0;
 }
