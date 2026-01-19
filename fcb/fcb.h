1f6b0a574bb16edbfaf460241009cf32056610f1
George Tsimpinis <gtsimpinis@b-k.gr>
2026-01-19 12:35:17 +0200

    feat: Add Fcb structure definition for Flash Circular Buffer logistics.

--- fcb/fcb.h
+++ fcb/fcb.h
@@ -15,6 +15,10 @@ typedef struct {
   uint32_t last_sector;  /**< Last sector index used by this FCB instance */
   uint32_t sector_size;  /**< Size of each sector in bytes */
   uint32_t current_sector_id; /**< Monotonic ID of the current active sector */
+  uint32_t write_addr;        /**< Next address to write new data to */
+  uint32_t read_addr;   /**< Address to start the next read operation from */
+  uint32_t delete_addr; /**< Address of the next item to be marked as consumed
+                           (deleted) */
 } Fcb;
 
 #endif // FCB_H
